194a9efdedaafbb482d5b3d28036ea866e231496
Jørgen Solli <jogex123@gmail.com>
2018-03-01 20:16:18 +0100

    Updated Vue. Added the first resource controller

---
+++ app/Http/Resources/Routine.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Logit\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Routine extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
